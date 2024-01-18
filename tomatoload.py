30fc4831f12e6f19fb39ebee1d15696f382b1df8
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-01-18 14:02:27 -0500

    tomato load function
    loaf

--- tomatoload.py
+++ tomatoload.py
@@ -1,7 +1,8 @@
 import time
 symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
-i = 0
-for tomato in range(20):
-    i = (i + 1) % len(symbols)
-    print('\r\033[K%s loading tomato...' % symbols[i], flush=True, end='')
-    time.sleep(0.1)
+def tomato_load(rep):
+    i = 0
+    for tomato in rep:
+        i = (i + 1) % len(symbols)
+        print('\r\033[K%s loading tomato...' % symbols[i], flush=True, end='')
+        time.sleep(0.1)
