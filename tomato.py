e247803a8bb7b40ae7bd0e23f76072a102fb3c25
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-01-18 09:25:29 -0500

    tomato.py

---
+++ tomato.py
@@ -0,0 +1,10 @@
+# tomato
+import time
+
+class tomato:
+  def __init__(self, name):
+    self.name = name
+
+name = input("[tomato]: enter name: ")
+tomato = tomato(name)
+print("[tomato]: initialized")
