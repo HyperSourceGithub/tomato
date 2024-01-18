39818245162690d79e18a6f2ea0341a24e92a8ed
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-01-18 18:57:17 +0000

    tomato update load thing

--- tomatoload.py
+++ tomatoload.py
@@ -1,7 +1,7 @@
 import time
 symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
 i = 0
-while True:
+for tomato in range(20):
     i = (i + 1) % len(symbols)
-    print('\r\033[K%s loading...' % symbols[i], flush=True, end='')
+    print('\r\033[K%s loading tomato...' % symbols[i], flush=True, end='')
     time.sleep(0.1)
