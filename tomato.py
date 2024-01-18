abddaf3c3474fe56055e5ad62b5afd3b15c84060
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-01-18 19:12:12 +0000

    tomato new stuff

--- tomato.py
+++ tomato.py
@@ -1,6 +1,15 @@
 # tomato
 import time
-from tomatoload.py import tomato_load
+import os
+
+# tomato load
+symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
+def tomato_load(rep):
+    i = 0
+    for tomato in range(rep):
+        i = (i + 1) % len(symbols)
+        print('\r\033[K%s loading tomato...' % symbols[i], flush=True, end='')
+        time.sleep(0.1)
 
 class tomato:
   def __init__(self, name, strand, main_weapon, secondary): # tomato strand is like class: mage, warrior, etc
@@ -9,9 +18,11 @@ class tomato:
     self.main = main_weapon
     self.side = secondary
 
-tomato_load(5)
+tomato_load(30)
+os.system('clear')
+print("tomato loaded.")
 name = input("[tomato]: enter name: ")
-strand = input("[tomato]: enter strand (warrior (slice & dice), mage (majik), ranger (arrow dude), spy (sneaky), operative (secret government agent)): ").lower().split()
+strand = input("[tomato]: enter strand (warrior (slice & dice), mage (majik), ranger (arrow dude), spy (sneaky), operative (secret government agent)): ").lower().strip()
 
 # tomato get strand and define main
 if strand == "warrior":
--- tomatoload.py
+++ tomatoload.py
@@ -2,7 +2,7 @@ import time
 symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
 def tomato_load(rep):
     i = 0
-    for tomato in rep:
+    for tomato in range(rep):
         i = (i + 1) % len(symbols)
         print('\r\033[K%s loading tomato...' % symbols[i], flush=True, end='')
         time.sleep(0.1)
